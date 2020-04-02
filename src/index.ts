6d52b13f7cc9bb73c2866c0edf449c05f1af09d2
Leo Decking <leo.decking@schoolyourself.de>
2020-04-02 23:01:14 +0200

    not working :()

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,3 @@ export { SignKey, VerifyKey, SecretKey } from "./Key";
 export { App } from "./App";
 export { Crypto } from "./Crypto";
 export { KeyStore } from "./KeyStore";
-export { verifyFirestore } from "./CloudFunctions";
