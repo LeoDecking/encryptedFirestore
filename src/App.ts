193ef58ecaf40466144520cb71a7573cd7ac398f
Leo Decking <leo.decking@schoolyourself.de>
2020-04-04 22:29:18 +0200

    realtime listeners, delete

--- src/App.ts
+++ src/App.ts
@@ -24,7 +24,16 @@ export class App {
         return (parent as App)[AppSymbol] == true;
     }
 
-    childFromFirestore<C extends DatabaseChildObjectType<App>>(child: new (parent: App, id: string) => C, id: string): Promise<C> {
-        return DatabaseObject.fromFirestore.call(child as any, this, id) as Promise<C>;
+
+    childFromFirestore<C extends DatabaseChildObjectType<App>>(child: new (parent: App, id?: string) => C, id: string): Promise<C>;
+    childFromFirestore<C extends DatabaseChildObjectType<App>>(child: new (parent: App, id?: string) => C, id: string, onSnapshot: (object: C) => void): () => void;
+    childFromFirestore<C extends DatabaseChildObjectType<App>>(child: new (parent: App, id?: string) => C, id: string, onSnapshot?: (object: C) => void): Promise<C> | (() => void) {
+        return DatabaseObject.fromFirestore.call(child as any, this, id, onSnapshot);
+    }
+
+    childrenFromFirestore<C extends DatabaseChildObjectType<App>>(child: new (parent: this, id?: string) => C, queries: { fieldPath: string | firebase.firestore.FieldPath, opStr: firebase.firestore.WhereFilterOp, value: any }[]): Promise<C[]>;
+    childrenFromFirestore<C extends DatabaseChildObjectType<App>>(child: new (parent: this, id?: string) => C, queries: { fieldPath: string | firebase.firestore.FieldPath, opStr: firebase.firestore.WhereFilterOp, value: any }[], onSnapshot: (objects: C[]) => void): () => void;
+    childrenFromFirestore<C extends DatabaseChildObjectType<App>>(child: new (parent: this, id?: string) => C, queries: { fieldPath: string | firebase.firestore.FieldPath, opStr: firebase.firestore.WhereFilterOp, value: any }[] = [], onSnapshot?: (objects: C[]) => void): Promise<C[]> | (() => void) {
+        return DatabaseObject.collectionFromFirestore.call(child as any, this, queries, onSnapshot);
     }
 }
--- src/DatabaseObject.ts
+++ src/DatabaseObject.ts
@@ -62,30 +62,46 @@ export abstract class DatabaseObject<Tstring extends string, T extends DatabaseO
 
         // console.log("encrypted", documentData);
         console.log("signed", await this.app.keyStore.sign(this.owner, documentData));
-        // TODO test if encrypted properties are there
         let signedObject = await this.app.keyStore.sign(this.owner, documentData);
 
         let result = await this.app.firebase.functions("europe-west3").httpsCallable("setDocument")(signedObject);
         if (result.data !== true) throw new Error("unkown error");
     }
 
-    async a() {
-        let b = await Job.collectionFromFirestore(null,[],()=>alert(42)).
-        b
+    async delete(): Promise<void> {
+        console.log("delete", this);
+        if(this.version == 0) throw new Error("trying to delete 0 version");
+
+        let documentData: any = {
+            path: this.path,
+            version: -this.version
+        };
+
+        console.log("signed", await this.app.keyStore.sign(this.owner, documentData));
+        let signedObject = await this.app.keyStore.sign(this.owner, documentData);
+
+        let result = await this.app.firebase.functions("europe-west3").httpsCallable("setDocument")(signedObject);
+        if (result.data !== true) throw new Error("unkown error");
     }
 
-    // TODO realtime in anyy method
-    
-    static async fromFirestore<T extends DatabaseObjectType>(this: new (parent: T["parent"], id?: string) => T, parent: T["parent"], id: string): Promise<T> {
+    static fromFirestore<T extends DatabaseObjectType>(this: new (parent: T["parent"], id?: string) => T, parent: T["parent"], id: string): Promise<T>;
+    static fromFirestore<T extends DatabaseObjectType>(this: new (parent: T["parent"], id?: string) => T, parent: T["parent"], id: string, onSnapshot: (object: T) => void): () => void;
+    static fromFirestore<T extends DatabaseObjectType>(this: new (parent: T["parent"], id?: string) => T, parent: T["parent"], id: string, onSnapshot?: (object: T) => void): Promise<T> | (() => void) {
         let dummy = new this(parent, id);
-        let documents = await DatabaseObject.collectionFromFirestore.call(this, parent, [{ fieldPath: "path", opStr: "==", value: dummy.path }]);
-        if (documents.length != 1) throw new Error("document not found");
-        return documents[0] as T;
+        if (!onSnapshot) {
+            return new Promise(async (resolve, reject) => {
+                let documents = await DatabaseObject.collectionFromFirestore.call(this, parent, [{ fieldPath: "path", opStr: "==", value: dummy.path }]);
+                if (documents.length != 1) reject("document not found");
+                else resolve(documents[0] as T);
+            });
+        } else {
+            return DatabaseObject.collectionFromFirestore.call(this, parent, [{ fieldPath: "path", opStr: "==", value: dummy.path }], ((objects: T[]) => onSnapshot(objects[0])));
+        }
     }
 
-    static async collectionFromFirestore<T extends DatabaseObjectType>(this: new (parent: T["parent"], id?: string) => T, parent: T["parent"], queries: { fieldPath: string | firebase.firestore.FieldPath, opStr: firebase.firestore.WhereFilterOp, value: any }[]): Promise<T[]>;
-    static async collectionFromFirestore<T extends DatabaseObjectType>(this: new (parent: T["parent"], id?: string) => T, parent: T["parent"], queries: { fieldPath: string | firebase.firestore.FieldPath, opStr: firebase.firestore.WhereFilterOp, value: any }[], onSnapshot: (objs: { obj: T, change?: firebase.firestore.DocumentChange<firebase.firestore.DocumentData> }[]) => void): Promise<(() => void)>;
-    static async collectionFromFirestore<T extends DatabaseObjectType>(this: new (parent: T["parent"], id?: string) => T, parent: T["parent"], queries: { fieldPath: string | firebase.firestore.FieldPath, opStr: firebase.firestore.WhereFilterOp, value: any }[] = [], onSnapshot?: (objs: { obj: T, change?: firebase.firestore.DocumentChange<firebase.firestore.DocumentData> }[]) => void): Promise<T[] | (() => void)> {
+    static collectionFromFirestore<T extends DatabaseObjectType>(this: new (parent: T["parent"], id?: string) => T, parent: T["parent"], queries: { fieldPath: string | firebase.firestore.FieldPath, opStr: firebase.firestore.WhereFilterOp, value: any }[]): Promise<T[]>;
+    static collectionFromFirestore<T extends DatabaseObjectType>(this: new (parent: T["parent"], id?: string) => T, parent: T["parent"], queries: { fieldPath: string | firebase.firestore.FieldPath, opStr: firebase.firestore.WhereFilterOp, value: any }[], onSnapshot: (objects: T[]) => void): (() => void);
+    static collectionFromFirestore<T extends DatabaseObjectType>(this: new (parent: T["parent"], id?: string) => T, parent: T["parent"], queries: { fieldPath: string | firebase.firestore.FieldPath, opStr: firebase.firestore.WhereFilterOp, value: any }[] = [], onSnapshot?: ((objects: T[]) => void)): Promise<T[]> | (() => void) {
         let dummy = new this(parent);
 
         let query: firebase.firestore.CollectionReference<firebase.firestore.DocumentData> | firebase.firestore.Query<firebase.firestore.DocumentData>
@@ -93,11 +109,15 @@ export abstract class DatabaseObject<Tstring extends string, T extends DatabaseO
         queries.forEach(q => query = query.where(q.fieldPath, q.opStr, q.value));
 
         if (!onSnapshot)
-            return await Promise.all((await query.get({ source: "server" })).docs.map(d => DatabaseObject.getObjectFromDocument.call(this, parent, d) as Promise<T>)) as T[];
+            return query.get({ source: "server" }).then(s => Promise.all(s.docs.map(d => DatabaseObject.getObjectFromDocument.call(this, parent, d) as Promise<T>)));
         else
             return query.onSnapshot(async s => {
-                let changes = s.docChanges();
-                onSnapshot((await Promise.all(s.docs.map(d => DatabaseObject.getObjectFromDocument.call(this, parent, d)))).map(o => ({ obj: o as T, change: changes.find(c => c.doc.id == o.id) })));
+                // type: : (objs: { obj: T, change?: firebase.firestore.DocumentChange<firebase.firestore.DocumentData> }[] => void
+                // let changes = s.docChanges();
+                // console.log(changes);
+                // console.log(s.docs);
+                // onSnapshot((await Promise.all(s.docs.map(d => DatabaseObject.getObjectFromDocument.call(this, parent, d)))).map(o => ({ obj: o as T, change: changes.find(c => c.doc.id == o.id) })));
+                onSnapshot(await Promise.all(s.docs.map(d => DatabaseObject.getObjectFromDocument.call(this, parent, d))) as T[]);
             }) as (() => void);
     }
 
@@ -123,11 +143,15 @@ export abstract class DatabaseObject<Tstring extends string, T extends DatabaseO
         return object as T;
     }
 
-    childFromFirestore<C extends DatabaseChildObjectType<this>>(child: new (parent: this, id?: string) => C, id: string): Promise<C> {
-        return DatabaseObject.fromFirestore.call(child as any, this, id) as Promise<C>;
+    childFromFirestore<C extends DatabaseChildObjectType<this>>(child: new (parent: this, id?: string) => C, id: string): Promise<C>;
+    childFromFirestore<C extends DatabaseChildObjectType<this>>(child: new (parent: this, id?: string) => C, id: string, onSnapshot: (object: C) => void): () => void;
+    childFromFirestore<C extends DatabaseChildObjectType<this>>(child: new (parent: this, id?: string) => C, id: string, onSnapshot?: (object: C) => void): Promise<C> | (() => void) {
+        return DatabaseObject.fromFirestore.call(child as any, this, id, onSnapshot);
     }
 
-    childrenFromFirestore<C extends DatabaseChildObjectType<this>>(child: new (parent: this, id?: string) => C, queries: { fieldPath: string | firebase.firestore.FieldPath, opStr: firebase.firestore.WhereFilterOp, value: any }[] = []): Promise<C[]> {
-        return DatabaseObject.collectionFromFirestore.call(child as any, this, queries) as Promise<C[]>;
+    childrenFromFirestore<C extends DatabaseChildObjectType<this>>(child: new (parent: this, id?: string) => C, queries: { fieldPath: string | firebase.firestore.FieldPath, opStr: firebase.firestore.WhereFilterOp, value: any }[]): Promise<C[]>;
+    childrenFromFirestore<C extends DatabaseChildObjectType<this>>(child: new (parent: this, id?: string) => C, queries: { fieldPath: string | firebase.firestore.FieldPath, opStr: firebase.firestore.WhereFilterOp, value: any }[], onSnapshot: (objects: C[]) => void): () => void;
+    childrenFromFirestore<C extends DatabaseChildObjectType<this>>(child: new (parent: this, id?: string) => C, queries: { fieldPath: string | firebase.firestore.FieldPath, opStr: firebase.firestore.WhereFilterOp, value: any }[] = [], onSnapshot?: (objects: C[]) => void): Promise<C[]> | (() => void) {
+        return DatabaseObject.collectionFromFirestore.call(child as any, this, queries, onSnapshot);
     }
 }
